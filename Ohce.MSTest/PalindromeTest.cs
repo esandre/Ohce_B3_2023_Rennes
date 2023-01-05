b7eb45013caaf3b1c4e6d52d15e45e7fb0ea6c1f
Enzo Sandré <atelier@enzosandre.fr>
2023-01-05 14:08:40 +0100

    Duplication des tests en MSTest

--- Ohce.MSTest/PalindromeTest.cs
+++ Ohce.MSTest/PalindromeTest.cs
@@ -1,12 +1,12 @@
-using FluentAssertions;
-using NFluent;
+using System.ComponentModel;
 
 namespace Ohce.Test;
 
+[TestClass]
 public class PalindromeTest
 {
-    [Fact(DisplayName = "QUAND on saisit une chaîne " +
-                        "ALORS celle-ci est renvoyée en miroir")]
+    [TestMethod("QUAND on saisit une chaîne " +
+                "ALORS celle-ci est renvoyée en miroir")]
     public void TestMiroir()
     {
         // QUAND on saisit une chaîne
@@ -15,24 +15,22 @@ public class PalindromeTest
 
         // ALORS celle-ci est renvoyée en miroir
         const string miroir = "otot";
-
-        Assert.Contains(miroir, résultat); // xUnit
-        résultat.Should().Contain(miroir); // FluentAssertions
-        Check.That(résultat).Contains(miroir); // NFluent
+        StringAssert.Contains(résultat, miroir);
     }
 
-    [Theory(DisplayName = "QUAND on saisit un palindrome " +
-                        "ALORS celui-ci est renvoyé " +
-                        "ET « Bien dit » est envoyé ensuite")]
-    [InlineData("Radar")]
-    [InlineData("radar")]
+    [DataTestMethod()]
+    [DisplayName("QUAND on saisit un palindrome " +
+                 "ALORS celui-ci est renvoyé " +
+                 "ET « Bien dit » est envoyé ensuite")]
+    [DataRow("Radar")]
+    [DataRow("radar")]
     public void TestPalindrome(string palindrome)
     {
         // QUAND on saisit un palindrome
         var résultat = DétectionPalindrome.Traiter(palindrome);
 
         // ALORS celui-ci est renvoyé
-        Assert.Contains(palindrome, résultat, StringComparison.CurrentCultureIgnoreCase);
+        StringAssert.Contains(résultat, palindrome, StringComparison.CurrentCultureIgnoreCase);
 
         var endOfPalindrome = résultat.IndexOf(palindrome, StringComparison.CurrentCultureIgnoreCase) 
                               + palindrome.Length;
@@ -40,34 +38,36 @@ public class PalindromeTest
         var résultatAprèsBienDit = résultat[endOfPalindrome..];
 
         // ET « Bien dit » est envoyé ensuite
-        Assert.StartsWith(Expressions.BienDit, résultatAprèsBienDit);
+        StringAssert.StartsWith(résultatAprèsBienDit, Expressions.BienDit);
     }
 
-    [Theory(DisplayName = "QUAND on saisit une chaîne " +
+    [DataTestMethod()]
+    [DisplayName("QUAND on saisit une chaîne " +
                         "ALORS « Bonjour » est envoyé avant toute réponse")]
-    [InlineData("")]
-    [InlineData("radar")]
-    [InlineData("toto")]
+    [DataRow("")]
+    [DataRow("radar")]
+    [DataRow("toto")]
     public void TestBonjour(string chaîneTestée)
     {
         // QUAND on saisit une chaîne
         var résultat = DétectionPalindrome.Traiter(chaîneTestée);
 
         // ALORS « Bonjour » est envoyé avant toute réponse
-        Assert.StartsWith(Expressions.Bonjour, résultat);
+        StringAssert.StartsWith(résultat, Expressions.Bonjour);
     }
 
-    [Theory(DisplayName = "QUAND on saisit une chaîne " +
-                        "ALORS « Au revoir » est envoyé en dernier")]
-    [InlineData("")]
-    [InlineData("radar")]
-    [InlineData("toto")]
+    [DataTestMethod]
+    [DisplayName("QUAND on saisit une chaîne " +
+                 "ALORS « Au revoir » est envoyé en dernier")]
+    [DataRow("")]
+    [DataRow("radar")]
+    [DataRow("toto")]
     public void TestAuRevoir(string chaîne)
     {
         // QUAND on saisit une chaîne
         var résultat = DétectionPalindrome.Traiter(chaîne);
 
         // ALORS « Au revoir » est envoyé en dernier
-        Assert.EndsWith(Expressions.AuRevoir, résultat);
+        StringAssert.EndsWith(résultat, Expressions.AuRevoir);
     }
 }
